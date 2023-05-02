055a9907802bf8dade054e38e2e25a22b238e370
zadohu22 <zadohu22@gmail.com>
2023-05-02 16:26:25 -0500

    test if template works properly

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { TestComponent } from './components/test-component.tsx'
 const App = () => {
   return (
     <div className="bg-gray-600 w-full h-full flex justify-center items-center">
-      <TestComponent hello="hello vite/typescript." />
+      <TestComponent hello="does the template work?" />
       
     </div>
   );
